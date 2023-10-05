359e05e030338644d6fc35f4a0faa65f7822093f
@AnZlycdy <chaudharyanjali756@gmail.com>
2023-10-05 13:37:04 +0545

    first commit

---
+++ let.js
@@ -0,0 +1,15 @@
+// carName = "Volvo";
+// var carName;
+// console.log(carName);
+
+// var myVar = 8;
+// myVar = myVar + 1;
+// console.log(myVar)
+
+var out = "shirt";
+function you() {
+    var out = "pant";
+    return out;
+}
+console.log(you());
+console.log(out);
---
+++ syntax.js
@@ -0,0 +1,18 @@
+// var myName = "Anjali"
+// myName = 8
+// console.log(myName)
+
+// var sum = 10+10
+// console.log(sum)
+
+// var difference = 10-8;
+// console.log(difference)
+
+// var product = 1*2;
+// console.log(product)
+
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder)
+
