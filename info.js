984cf5d2dd076a31f62d4f17859dca8f8c4cd82d
@AnZlycdy <chaudharyanjali756@gmail.com>
2023-10-06 12:06:39 +0545

    first commit

---
+++ info.js
@@ -0,0 +1,15 @@
+
+
+const person = {
+    name:"apexa",
+    age:"20",
+    car:"tesla",  
+    
+}
+
+function display(detail){
+    return(`Hello ${detail.name} is ${detail.age} years old having ${detail.car}.`)
+
+}
+console.log(display(person));
+
