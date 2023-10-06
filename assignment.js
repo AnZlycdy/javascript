2323de59848d75d36eb270f18c33b881c3807daa
@AnZlycdy <chaudharyanjali756@gmail.com>
2023-10-06 13:38:13 +0545

    first commit

---
+++ assignment.js
@@ -0,0 +1,16 @@
+// let x = 10;
+// x += 5;
+// console.log(x);
+
+// let x = 7; 
+//   x <<= 2;   
+//   console.log(x);
+
+// let x = 5; 
+//   x <<= 1;   
+//   console.log(x);
+
+//   let x = 100; 
+//   x <<= 5;   
+//   console.log(x);
+
